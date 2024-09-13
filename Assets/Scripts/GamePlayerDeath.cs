7f04279ed30f3dc1f269ed26e0c5b2cac4ccef87
Audrey Man <82979811+MournTHEMorning@users.noreply.github.com>
2024-09-12 23:51:18 -0400

    Added Events to Collisions and Death
    OnPlayerDies (Death), OnChangeToLane<Letter> (Movement), OnRiffCollision(Collisons) added respectively

--- Assets/Scripts/GamePlayerDeath.cs
+++ Assets/Scripts/GamePlayerDeath.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class GamePlayerDeath : MonoBehaviour
 {
+
+    [Header("Events")]
+    public UnityEvent OnPlayerDies;
+
+    [Header("Debug")]
     public bool ActivateConsole;
 
     internal void OnTriggerEnter(Collider other){
         if(other.gameObject.CompareTag("Enemy")){
             if(ActivateConsole){Debug.Log("Player was hit!");}
+            OnPlayerDies.Invoke();
+
             Destroy(this.gameObject); 
         }
     }
--- Assets/Scripts/GamePlayerMovement.cs
+++ Assets/Scripts/GamePlayerMovement.cs
@@ -9,6 +9,14 @@ public class GamePlayerMovement : MonoBehaviour
     // public Player playerInputMap;
     // private InputAction move;
 
+    [Header("Events")]
+    public UnityEvent OnChangeToLaneF;
+    public UnityEvent OnChangeToLaneG;
+
+    public UnityEvent OnChangeToLaneH;
+    public UnityEvent OnChangeToLaneJ;
+
+
     #region Lane Positions
     [Header("Lanes to Traverse")]
     public Transform fPosition;
@@ -17,7 +25,11 @@ public class GamePlayerMovement : MonoBehaviour
     public Transform jPosition;
     #endregion
 
+
+    #region Player Defaults
+    //TODO - make it so player can start from any lane
     private Vector3 defaultPlayerPosition;
+    #endregion
 
 
     #region Console Stuff 
@@ -66,21 +78,29 @@ public class GamePlayerMovement : MonoBehaviour
         //TODO: the below WITH the new input system: https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/manual/QuickStartGuide.html
         if(Keyboard.current.fKey.wasPressedThisFrame){
             if(ActivateConsole){Debug.Log("F Key pressed");}
+            OnChangeToLaneF.Invoke();
+
             this.transform.position = new Vector3(fPosition.transform.position.x, defaultPlayerPosition.y, defaultPlayerPosition.z);
 
         }
         else if(Keyboard.current.gKey.wasPressedThisFrame){
             if(ActivateConsole){Debug.Log("G Key was pressed");}
+            OnChangeToLaneG.Invoke();
+
             this.transform.position = new Vector3(gPosition.transform.position.x, defaultPlayerPosition.y, defaultPlayerPosition.z);
         }
 
         else if(Keyboard.current.hKey.wasPressedThisFrame){
              if(ActivateConsole){Debug.Log("H was pressed");}
+                OnChangeToLaneH.Invoke();
+
             this.transform.position = new Vector3(hPosition.transform.position.x, defaultPlayerPosition.y, defaultPlayerPosition.z);
 
         }
         else if(Keyboard.current.jKey.wasPressedThisFrame){
              if(ActivateConsole){Debug.Log("J was pressed");}
+             OnChangeToLaneJ.Invoke();
+
             this.transform.position = new Vector3(jPosition.transform.position.x, defaultPlayerPosition.y, defaultPlayerPosition.z);
         }
 
