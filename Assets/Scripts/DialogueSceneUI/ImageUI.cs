23ae5467e86a044750834d593f97d5f8cc05d441
Audrey Man <82979811+MournTHEMorning@users.noreply.github.com>
2024-09-13 06:36:54 -0400

    DoubleCharacterDialogue not able in given time; Did bg image slideshow
    -moved text up, removed name
    -added image flipbook (ImageUI)

--- Assets/Scripts/DialogueSceneUI/ImageUI.cs
+++ Assets/Scripts/DialogueSceneUI/ImageUI.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ImageUI : MonoBehaviour
 {
+    [Header("Images, Background Images")]
+    public Sprite[] images;
 
-    //this is how you grab that list shit in unity :)
-    public Sprite[] lines;
+    public Image currentImage;
 
-    // Start is called before the first frame update
+    private int index = 0;
+
     void Start()
     {
-        
+        currentImage = GetComponent<Image>();
+
+        if (images.Length > 0)
+        {
+            ShowImage(index); // Show the first image
+        }
+        else
+        {
+            Debug.Log("ImageUI has been disabled; no bg images");
+            this.enabled = false;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+       if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            index++;
+            if (index >= images.Length) // Check if index is out of bounds
+            {
+                gameObject.SetActive(false);
+                // if you want to load the next scene
+                // SceneManager.LoadScene(nextScene);
+            }
+            else
+            {
+                ShowImage(index);
+            }
+        }
+    }
+
+    void ShowImage(int index)
+    {
+        if (currentImage != null && index >= 0 && index < images.Length)
+        {
+            Debug.Log($"currentImage: {currentImage} || sprite: {currentImage.sprite}");
+            currentImage.sprite = images[index];
+        }
+        else
+        {
+            Debug.LogError("Invalid index or currentImage is not assigned.");
+        }
     }
 }
